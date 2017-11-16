a618444d0ade912fa9efc28cc2ba47cde90ec8f7
Darren Homrighausen <dhomrigh@gmail.com>
2017-11-15 23:17:20 -0600

    adding gradient descent code

--- neuralNetworks/neuralNetworks_gradientDescent.R
+++ neuralNetworks/neuralNetworks_gradientDescent.R
@@ -2,21 +2,21 @@
 ##############################
 ##############################
 set.seed(1)
-p = 2
-n = 10
-X = matrix(rnorm(n*p),nrow=10)
-b = c(10,1)
+p = 3
+n = 100
+X = matrix(rnorm(n*p),nrow=n)
+b = 1:p
 Y = X%*%b + rnorm(n)
 
 
 #Stochastic Gradient Descent
-miniBatchParm = 5
+miniBatchParm = n/2
 
-muF = function(X,b){
+fF = function(X,b){
   return( X %*% b)
 }
-ellF = function(Y,mu){
-  return((Y - mu)**2)
+ellF = function(Y,f){
+  return((Y - f)**2)
 }
 rF = function(ell){
   return( mean( ell ))
@@ -27,19 +27,19 @@ bHat          = rnorm(p)
 nIter         = 1000
 learnRate     = .01
 
-bHatIter      = matrix(0,nrow=nIter,ncol=2)
+bHatIter      = matrix(0,nrow=nIter,ncol=p)
 #Step 1
 for(iter in 1:nIter){
   batch = sample(1:n,miniBatchParm,replace=FALSE)
   for(j in 1:p){
     #Step 2a:Forward
-    mu  = muF(X[batch,],bHat)
-    ell = ellF(Y[batch],mu)
+    f  = fF(X[batch,],bHat)
+    ell = ellF(Y[batch],f)
     r   = rF(ell)
     #Step 2b:Backward
-    dell_dmu = -2*(Y[batch] - mu)
-    dmu_dbj  = X[batch,j]
-    dR_dbj   = rF( dell_dmu*dmu_dbj )
+    dell_df = -2*(Y[batch] - f)
+    df_dbj  = X[batch,j]
+    dR_dbj   = rF( dell_df*df_dbj )
     #Step 3: Update
     bHat[j]  = bHat[j] - learnRate * dR_dbj
   }
@@ -50,5 +50,11 @@ for(iter in 1:nIter){
 bHat_LS = lm(Y~X-1)$coef
 print(bHat_LS)
 
-plot(bHatIter,col=rainbow(nIter))
+plot(bHatIter[,1:2],col=rainbow(nIter),xlab='beta1',ylab='beta2')
 points(bHat_LS[1],bHat_LS[2],col='black',pch=2)
+
+require(scatterplot3d)
+out.plot = scatterplot3d(bHatIter[,1:3],color=rainbow(nIter),
+                         xlab='beta1',ylab='beta2',zlab='beta3')
+                        
+out.plot$points3d(bHat_LS[1],bHat_LS[2],bHat_LS[3],col='black',pch=2)
